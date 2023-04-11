55cb887071bdb44af5ecb21eeb43c73d6aafd326
Tom Brereton <tbrereton89@gmail.com>
2023-04-11 15:40:44 +1000

    add link to result card

--- src/components/ResultCard.tsx
+++ src/components/ResultCard.tsx
@@ -8,18 +8,20 @@ var options = {
 
 export default function ResultCard(post: Document): JSX.Element {
   return (
-    <article
-      key={post.slugLink}
-      className="bg-black border border-black-light rounded px-5 py-10 flex flex-col gap-10"
-    >
-      <div>
-        <h2 className="font-sans text-xl">{post.title}</h2>
-        <h3 className="font-mono">
-          {post.dateCreated.toLocaleString("en-GB", options)}
-        </h3>
-      </div>
-      <p>{post.summary}</p>
-      <p className="font-mono">{post.tags.join(" ")}</p>
-    </article>
+    <a href={"/blog/" + post.slugLink}>
+      <article
+        key={post.slugLink}
+        className="bg-black border border-black-light rounded px-5 py-10 flex flex-col gap-10"
+      >
+        <div>
+          <h2 className="font-sans text-xl">{post.title}</h2>
+          <h3 className="font-mono">
+            {post.dateCreated.toLocaleString("en-GB", options)}
+          </h3>
+        </div>
+        <p>{post.summary}</p>
+        <p className="font-mono">{post.tags.join(" ")}</p>
+      </article>
+    </a>
   );
 }
