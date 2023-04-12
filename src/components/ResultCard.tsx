b4396812c82508876d148ac287cb691e58ce4b02
Tom Brereton <tbrereton89@gmail.com>
2023-04-12 17:01:21 +1000

    Add icon to result card

--- src/components/ResultCard.tsx
+++ src/components/ResultCard.tsx
@@ -1,4 +1,5 @@
 import type { Document } from "@/content/schemas/_document";
+import { Hammer, ArticleMedium } from "@phosphor-icons/react";
 
 var options: Intl.DateTimeFormatOptions = {
   year: "numeric",
@@ -10,10 +11,14 @@ export default function ResultCard(post: Document): JSX.Element {
   return (
     <article
       key={post.slugLink}
-      className="bg-black border border-black-light rounded px-5 py-10 flex flex-col gap-10"
+      className="bg-black border border-black-light rounded px-5 pb-5 flex flex-col gap-10"
     >
-      <a href={"/blog/" + post.slugLink}>
+      <a href={"/blog/" + post.slugLink} className="flex flex-col gap-10">
         <div>
+          <div className="flex w-full justify-end mt-5 mb-3">
+            {post.category === "post" && <ArticleMedium size={22} />}
+            {post.category === "project" && <Hammer size={22} />}
+          </div>
           <h2 className="font-sans text-xl">{post.title}</h2>
           <h3 className="font-mono">
             {post.dateCreated.toLocaleString("en-GB", options)}
