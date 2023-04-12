ac1d3a12a086a336b5735b11899b2928ed214864
Tom Brereton <tbrereton89@gmail.com>
2023-04-12 14:23:13 +1000

    Add query string parameter for search

--- src/components/ResultCard.tsx
+++ src/components/ResultCard.tsx
@@ -1,6 +1,6 @@
 import type { Document } from "@/content/schemas/_document";
 
-var options = {
+var options: Intl.DateTimeFormatOptions = {
   year: "numeric",
   month: "long",
   day: "numeric",
@@ -8,11 +8,11 @@ var options = {
 
 export default function ResultCard(post: Document): JSX.Element {
   return (
-    <a href={"/blog/" + post.slugLink}>
-      <article
-        key={post.slugLink}
-        className="bg-black border border-black-light rounded px-5 py-10 flex flex-col gap-10"
-      >
+    <article
+      key={post.slugLink}
+      className="bg-black border border-black-light rounded px-5 py-10 flex flex-col gap-10"
+    >
+      <a href={"/blog/" + post.slugLink}>
         <div>
           <h2 className="font-sans text-xl">{post.title}</h2>
           <h3 className="font-mono">
@@ -20,8 +20,16 @@ export default function ResultCard(post: Document): JSX.Element {
           </h3>
         </div>
         <p>{post.summary}</p>
-        <p className="font-mono">{post.tags.join(" ")}</p>
-      </article>
-    </a>
+      </a>
+      <div className="flex gap-4 font-mono">
+        {post.tags.map((tag) => {
+          return (
+            <a key={tag} href={`/search?q=%3D${tag}`}>
+              {tag}
+            </a>
+          );
+        })}
+      </div>
+    </article>
   );
 }
--- src/components/SearchResults.tsx
+++ src/components/SearchResults.tsx
@@ -1,7 +1,7 @@
 import type { Document } from "@/content/schemas/_document";
 import type Fuse from "fuse.js";
 import DocumentRepository from "@/functions/DocumentRepository";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import ResultCard from "./ResultCard";
 
 type Props = {
@@ -10,22 +10,26 @@ type Props = {
 
 let config: Fuse.IFuseOptions<Document> = {
   keys: ["title", "tags", "summary", "category"],
-  useExtendedSearch: true
+  useExtendedSearch: true,
 };
 
 export default function SearchResults({ documents }: Props): JSX.Element {
   const [query, setQuery] = useState("");
+
+  useEffect(() => {
+    const params = new URLSearchParams(window.location.search); // id=123
+    const queryFromParams = params.get("q");
+    setQuery(queryFromParams || "");
+  }, []);
+
   const docRepo = new DocumentRepository(documents, config);
-  const results = docRepo.search(query);
+  let results = docRepo.search(query);
 
   function handleText(event: any) {
     const value = event.target.value;
-    const searchPrompt = document.getElementById("search-prompt");
-    if (searchPrompt) {
-      searchPrompt.style.display = "none";
-    }
     setQuery(value);
   }
+
   return (
     <div className="flex flex-col w-full">
       <input
@@ -37,14 +41,16 @@ export default function SearchResults({ documents }: Props): JSX.Element {
         value={query}
         aria-label="document-search-input"
       />
-      <div>
-        <h2
-          id="search-prompt"
-          className="font-serif text-3xl italic text-center mt-12 m-3 "
-        >
-          Search for tags, posts, or projects.
-        </h2>
-      </div>
+      {results.length === 0 && (
+        <div>
+          <h2
+            id="search-prompt"
+            className="font-serif text-3xl italic text-center mt-12 m-3 "
+          >
+            Search for tags, posts, or projects.
+          </h2>
+        </div>
+      )}
       <div role="listbox" className="flex flex-col gap-5 h-full">
         {results.map((post) => {
           return ResultCard(post);
